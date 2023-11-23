6b499ba563c3d39882432cf7d7e687db133ed508
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-23 10:09:12 +0530

    modified:   public/index.html 	modified:   src/App.js 	modified:   src/components/Features.js 	new file:   src/images/chat-icon.svg 	new file:   src/images/database-icon.svg 	new file:   src/images/email-icon.svg 	new file:   src/images/terminal-icon.svg 	modified:   src/index.css 	modified:   tailwind.config.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,13 +1,24 @@
 import React from "react";
 
+const Features = () => {
+  return (
+    <div className="font-poppins">
+        <div className="ww-full min-h-screen p-6">
+            <h1 className="font-bold text-xl text-center">Check out the <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Features</span> of our Website</h1>  
 
-const Features = ( { title, name } ) => {
-    return (
-        <features>
-            <h1>{title}</h1>
-            <h1>{name}</h1>
-        </features>
-    )
-} 
+            <div className="flex">
+
+                <div>
+
+                    <h2>DataBase Support</h2>
+                    <a href="#">Know More</a>
+                    
+                </div>    
+            </div>  
+        </div>  
+      
+    </div>
+  )
+}
 
 export default Features;
